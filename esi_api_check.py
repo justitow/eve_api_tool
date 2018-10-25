5bc071589d3c14f2bcd505586faefe2fd5ce8b73
Weich <jweich01@blueshieldca.com>
2018-10-25 16:25:59 -0700

    api request and database loading complete

--- esi_api_check.py
+++ esi_api_check.py
@@ -22,6 +22,93 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 ########################################################################
 
+
+def generate_market_requests(input_list, order_string, region):
+    operations = []
+    for input_id in input_list:
+        operations.append(
+            api.app.op['get_markets_region_id_orders'](
+                region_id=region,
+                page=1,
+                order_type=order_string,
+                type_id=input_id[0],
+            )
+        )
+        '''
+        res = api.client.head(operation)
+        if res.status==200:
+            number_of_pages = res.header['X-Pages'][0]
+            if number_of_pages > 1:
+                print("wweeeeooooweeeeeeooooo")
+            for page in range(1, number_of_pages+1):
+                operations.append(
+                    api.app.op['get_markets_region_id_orders'](
+                        region_id=10000002,
+                        page=page,
+                        order_type=order_string,
+                        type_id=input_id[0],
+                    )
+                )
+        '''
+    return operations
+def to_sql_datetime(in_date):
+    outstring = str(in_date.year)+'-'+str(in_date.month)+'-'+str(in_date.day)+' '+str(in_date.hour)+':'+str(in_date.minute) + ':' + str(in_date.second) 
+    return outstring
+def fetch_market_data(cur):
+    cur.execute("SELECT DISTINCT materialTypeID FROM reaction_materials")
+    materials=cur.fetchall()
+    cur.execute("SELECT DISTINCT productTypeID FROM reaction_products")
+    products=cur.fetchall()
+    
+    operations = []
+    regions = [10000002, 10000042, 10000043, 10000032]
+    for region in regions:
+        operations += generate_market_requests(materials, 'sell', region)
+        operations += generate_market_requests(products, 'buy', region)
+    
+    
+    results = None
+    print('starting request')
+    if len(operations) > 1:
+        results = api.client.multi_request(operations)
+    else:
+        results = api.client.request(operations[0])
+    #print(results)
+    return results
+    
+def initialize_database():
+    con = sqlite3.connect('./program.sqlite')
+    con.isolation_level = None
+    cur = con.cursor()
+    cur.execute("ATTACH DATABASE 'sde.sqlite' AS sde")
+    return cur
+    
+def insert_results(results, cur):
+    print('inserting')
+    cur.execute("DELETE FROM market_info")
+    cur.execute('BEGIN TRANSACTION')
+    for swagger_object in results:
+        for order in swagger_object[1].data:
+            cur.execute('INSERT OR IGNORE INTO market_info (duration,is_buy_order, location_id, min_volume, order_id, price, range, system_id, type_id, volume_remain, volume_total) VALUES (?,?,?,?,?,?,?,?,?,?,?)', 
+            (order['duration'],
+             #order['issued'],
+             order['is_buy_order'],
+             order['location_id'],
+             order['min_volume'],
+             order['order_id'],
+             order['price'],
+             order['range'],
+             order['system_id'],
+             order['type_id'],
+             order['volume_remain'],
+             order['volume_total']))
+    cur.execute('COMMIT')
+             
+ 
+def chunks(data, rows=10000):
+    for i in range(0, len(data), rows):
+        yield data[i:i+rows]
+
 class API:
     def __init__(self):
         self.esi_app = EsiApp()
@@ -33,7 +120,7 @@ class API:
         self.app = None
         if difference.days > 1:
             print('Old swagger information, reinitializing app')
-            self.app = esi_app.get_latest_swagger
+            self.app = self.esi_app.get_latest_swagger
             with open('app.p', 'wb') as handle:
                 pickle.dump(self.app, handle, protocol=pickle.HIGHEST_PROTOCOL)
         else:
@@ -60,28 +147,9 @@ if __name__=="__main__":
     #esi_app, app, client = initialize_api_handlers()
     api = API()
     
+    cur = initialize_database()
     
-    print('Requesting api information')
-    market_order_operation = api.app.op['get_markets_region_id_orders'](
-        region_id=10000002,
-        type_id=34,
-        order_type='all',
-    )
+    results = fetch_market_data(cur)
     
-    
-
-    # do the request
-    response = api.client.request(market_order_operation)
-    
-    # use it: response.data contains the parsed result of the request.
-    print(response.data[0].price)
-
-    # to get the headers objects, you can get the header attribute
-    print(response.data[0])
-    
-    
-    
-    con = sqlite3.connect('./program.sqlite')
-    con.isolation_level = None
-    cur = con.cursor()
-    cur.execute("ATTACH DATABASE 'sde.sqlite' AS sde")
+    insert_results(results, cur)
+    print(results)
